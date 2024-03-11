9ac7d4059f8c3b5bb8dedbec58d053d021262988
David Salter <davidmsalter@me.com>
2024-03-11 23:00:17 +0000

    WiP: Database functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,32 +9,32 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-    app.notFound(w)
+		app.notFound(w)
 		return
 	}
 
-  files := []string {
-    "./ui/html/base.tmpl",
-    "./ui/html/partials/nav.tmpl",
-    "./ui/html/pages/home.tmpl",
-  }
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-    app.serverError(w, r, err)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-    app.serverError(w, r, err)
+		app.serverError(w, r, err)
 	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-    app.notFound(w)
+		app.notFound(w)
 		return
 	}
 
@@ -45,8 +45,17 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		app.clientError(w, http.StatusMethodNotAllowed)
-    return
+		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
