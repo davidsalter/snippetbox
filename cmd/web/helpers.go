3e465244bd25127599dbffabb3a4b0331a241843
David Salter <davidmsalter@me.com>
2024-03-16 22:26:35 +0000

    Complete create form

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
+
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecoderError *form.InvalidEncodeError
+
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+	return nil
+}
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/davidsalter/snippetbox
 go 1.22.0
 
 require (
+	github.com/go-sql-driver/mysql v1.8.0 // direct
+	github.com/julienschmidt/httprouter v1.3.0 // direct
+	github.com/justinas/alice v1.2.0 // direct
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.0 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
+	github.com/go-playground/form/v4 v4.2.1 // indirect
 )
